47bd8dea652611fdef5c0855c3f4a443631c443c
Johnny Edwards <jedwards@ziggo.nl>
2016-02-14 23:42:55 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ shinyUI(pageWithSidebar(
   
         sidebarPanel(
             helpText(paste(
-                "This app display the impact of of severe weather events in the US in the period 1996 thru 2011"
+                "This app displays the impact of severe weather events in the US in the period 1996 thru 2011"
                 , "The data is from the US NOAA storm database."
                 )),
             
@@ -14,7 +14,7 @@ shinyUI(pageWithSidebar(
 
             hr(),
             hr(),
-
+             
             selectInput("var", label = "Choose a variable to display", 
                         choices = list("fatalities"              = "fatalities",
                                        "injuries"               = "injuries" ,
@@ -24,7 +24,7 @@ shinyUI(pageWithSidebar(
             ),
             
            conditionalPanel(
-                condition = "input.tab != 'Overview'",
+                condition = "input.tab == 'Map'",
                 selectInput("type", label = "Choose a weather type", 
                         choices = list("<ALL>",
                                        "COLD","DRY","FLOOD","HEAT","HURRICANE","LIGHTNING",
@@ -40,9 +40,29 @@ shinyUI(pageWithSidebar(
         tabsetPanel(type = "tabs", id="tab",
             tabPanel("Overview", tabName="Main", plotOutput("ggtotals")), 
             tabPanel("Map",      tabName="Map",  
-                     verbatimTextOutput("maptitle1"),
-                     verbatimTextOutput("maptitle2"),
-                     htmlOutput("gvis")  )
+                     h3(textOutput("maptitle1")),
+                     h4(textOutput("maptitle2")),
+                     htmlOutput("gvis")  ),
+            
+            tabPanel("Help",      tabName="Help",  
+
+                     h3("General"),
+                     helpText(paste(
+                         "There are 2 tabs. The first contains a boxplot summarizing the total impact "
+                         , "of different types of weather. The second is a map showing the "
+                         , "impact per state of the US."
+                     )),
+
+                     h3("Drop down options"),
+                     
+                     helpText(paste(
+                         "By default the numbers are fatalities for "
+                         , "all types of weather. This can be altered "
+                         , "by choising different options in the drop down "
+                         , "boxes in the left hand panel."
+                     ))
+            )
+            
         )
     )    
     
